b9e6e16bb9e401ab4d24fbea9399613cd43fa7a7
Mikhaylenko Egor <zhora.mikhaylenko@mail.ru>
2022-02-28 09:59:56 +0700

    empty commit. git registered changes that were not

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void print_arena(char (*arena)[ARENA_SIZE]) {
-	cout << "ARENA" << endl;
+	cout << "ARENA:" << endl;
     for (int i = 0; i < ARENA_SIZE; i++) {
         for (int j = 0; j < ARENA_SIZE; j++) {
             cout << arena[i][j];
